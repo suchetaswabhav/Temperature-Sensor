7b809283af5bb0999b99ce24f13bc43ac21f57e9
suchetaswabhav <shrivastavasucheta@gmail.com>
2019-09-12 21:20:46 +0530

    initial commit

--- server/service/process-Json.js
+++ server/service/process-Json.js
@@ -1,35 +1,38 @@
 var fs = require('fs');
-var Q = require ('q');
+var Q = require('q');
 var obj = {};
-var tsValue ;
 var responseData = [];
 
 var myModuleJSON = {
-	defineRoutes: async function (router, filePath) {
+    defineRoutes: async function (router, filePath) {
         var deferred = Q.defer();
-        // var obj = JSON.parse(fs.readFileSync('file', 'utf8')); //original
-obj = {};
-         obj =await  JSON.parse(fs.readFileSync(filePath, 'utf8'));
+        obj = {};
+        obj = await JSON.parse(fs.readFileSync(filePath, 'utf8'));
 
-        console.log("JSON Object = ", obj)
         responseData = [];
 
-        tsValue = obj[0].ts
-        for (var i=0; i <obj.length ; i++){
-          if(obj[i].ts == tsValue){
-              responseData.push({ts:obj[i].ts , val: obj[i].val, year: 1920})
-          }
+        const map = new Map();
+        for (const item of obj) {
+            var year = new Date(item.ts).getFullYear()
+            if (!map.has(year)) {
+                map.set(year, true);    // set any value to Map
+                responseData.push({
+                    ts: item.ts,
+                    val: item.val,
+                    year: year
+                });
+            }
         }
+
+
         console.log("responseData = ", responseData)
         deferred.resolve(responseData);
         return deferred.promise;
     }
 
-    
 }
 
 
-    // ---------- automate run ends  ----------------
-    
-    module.exports = myModuleJSON;
-    
+// ---------- automate run ends  ----------------
+
+module.exports = myModuleJSON;
